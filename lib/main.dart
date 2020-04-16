8088b1a162f1548d2b373a991a55e618fb37fbf4
bhogalprabjot <bhogalprabjot1@gmail.com>
2020-04-16 15:55:36 +0530

    Overview screen added.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:shop_app/screens/products_overview_screen.dart';
 
 void main() => runApp(ShopApp());
 
 class ShopApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      // title: Text("Shop App"),
+      // child: Center(
+      //   child: Text("This is a shop app"),
+      // ),
+      home: ProductOverviewScreen(),
+      
+    );
   }
 }
