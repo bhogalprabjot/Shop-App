00654a76738f66872160572444a3eaf3801128fc
bhogalprabjot <bhogalprabjot1@gmail.com>
2020-04-16 15:19:24 +0530

    Shop App

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(ShopApp());
+
+class ShopApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
